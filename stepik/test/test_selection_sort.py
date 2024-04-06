c0e78a63614df0d4e5330a9a5b1615cdc2a93234
Maria <zaush@bk.ru>
2024-04-06 23:41:18 +0300

    updated tests

--- stepik/test/test_selection_sort.py
+++ stepik/test/test_selection_sort.py
@@ -1,12 +1,21 @@
 import unittest
+import random
 from selection_sort.selection_sort import selection_sort
 
 class TestSum(unittest.TestCase):
 
     def test_selection_sort(self):
-        self.assertEqual(selection_sort([7, 8, 2, 5, 3, 6, 1, 9]), [1, 2, 3, 5, 6, 7, 8, 9])
-        self.assertEqual(selection_sort([1, 2, 3, 4, 5, 6, 7, 8]), [1, 2, 3, 4, 5, 6, 7, 8])
-        self.assertEqual(selection_sort([2, 8, 7, 5, 3, 6]), [2, 3, 5, 6, 7, 8])
+        random_list = self.generate()
+        sorted_list = sorted(random_list)
+
+        self.assertEqual(selection_sort(random_list), sorted_list)
+
+    def generate(self) -> list:
+        random_list = []
+        for i in range(0, 100):
+            n = random.randint(1, 1_000_000)
+            random_list.append(n)
+        return random_list
 
 if __name__ == '__main__':
     unittest.main()
